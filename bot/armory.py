2a9412f2eae74b86261cca3b0d8f4a2cca2305b2
Lego-RL <joeylbarroso@gmail.com>
2023-01-31 18:27:22 -0600

    basic armory command implementations

--- bot/armory.py
+++ bot/armory.py
@@ -4,19 +4,105 @@ from discord import ApplicationContext
 from discord.commands import slash_command
 from discord.ext import commands
 
+import json
+import os
+
+def get_player_snapshots() -> list[dict]:
+
+    snapshots: list[dict] = []
+
+    files: list[str] = os.listdir("playerSnapshots")
+    
+    for file in files:
+        with open(f"playerSnapshots/{file}", "r") as f:
+            snapshots.append(json.load(f))
+
+    return snapshots
+
+def get_player_stats(ign: str) -> dict:
+    """
+    Return dictionary of player stats, given a player name.
+    """
+
+    snapshots: list[dict] = get_player_snapshots()
+    to_preserve: list[str] = ["vaultLevel", "powerLevel", "abilities", "talents", "researches"]
+
+    for snapshot in snapshots:
+        if snapshot["playerNickname"] == ign:
+            stats: dict = {key: snapshot[key] for key in snapshot if key in to_preserve}
+            return stats
+
+    return {}
+
+    
+
 class Armory(commands.Cog):
     def __init__(self, bot: discord.Bot) -> None:
         self.bot: discord.Bot = bot
 
-    
+
+    @slash_command(name="stats")
+    async def stats(self, ctx: ApplicationContext, ign: str):
+        """
+        Respond with an embed of player stats on the requested player.
+        """
+
+        stats: dict = get_player_stats(ign)
+
+        if not stats:
+            await ctx.respond("Could not find a player with given username!",)
+            return
+        
+        vaultLevel, powerLevel = stats["vaultLevel"], stats["powerLevel"]
+        abilities_str: str = ""
+        talents_str: str = ""
+        researches_str: str = ""
+
+        for ability, level in stats["abilities"].items():
+            abilities_str += f"{ability}: {level}\n"
+
+        for talent, level in stats["talents"].items():
+            talents_str += f"{talent}: {level}\n"
+
+        for research in stats["researches"]:
+            researches_str += f"{research}\n"
+
+
+        description: str = f"Vault level: **{vaultLevel}**\nPower level: **{powerLevel}**"
+        embed: discord.Embed = discord.Embed(title=f"{ign}'s Stats", description=description)
+
+        embed.add_field(name="Abilities", value=abilities_str, inline=True)
+        embed.add_field(name='Talents', value=talents_str, inline=True)
+        embed.add_field(name='Researches', value=researches_str, inline=False)
+
+        await ctx.respond(embed=embed)
+            
+
+
+
     @slash_command(name="in-vault")
     async def invault(self, ctx: ApplicationContext):
         """
         Respond with a list of players currently in a vault.
         """
 
-        
+        snapshots: list[dict] = get_player_snapshots()
+        players_in_vault: list[str] = []
 
+        for snapshot in snapshots:
+            if snapshot["inVault"]:
+                players_in_vault.append(snapshot["playerNickname"])
+
+        if players_in_vault:
+            await ctx.respond("Players currently in vault:\n" + "\n".join(players_in_vault))
+
+        else:
+            await ctx.respond("There are currently no players in a vault!")
+
+
+
+if __name__ == "__main__":
+    pass
 
 
 def setup(bot: discord.Bot) -> None:
