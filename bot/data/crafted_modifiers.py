67b6114f366d829e516695fb615e0085ab4c0cd4
jarnevermant <r0836442@student.thomasmore.be>
2023-03-24 16:06:38 +0100

    attempt 2 at fixing cm data reading

--- bot/data/crafted_modifiers.py
+++ bot/data/crafted_modifiers.py
@@ -72,7 +72,7 @@ def get_crafted_modifiers(username: str):
     crafted_modifiers: list = []
 
     # Add data to crafted modifiers list
-    crafted_modifiers.append(get_crafted_modifiers_data[player_uuid])
+    crafted_modifiers.append(get_crafted_modifiers_data().get(player_uuid))
 
     # Return data
     return crafted_modifiers
